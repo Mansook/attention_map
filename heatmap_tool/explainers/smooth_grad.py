a9725f8988bca0de3a28230af3b79154cf71861c
Mansook <106676599+Mansook@users.noreply.github.com>
2025-07-21 14:02:23 +0900

    slic on Smooth Grad

--- heatmap_tool/explainers/smooth_grad.py
+++ heatmap_tool/explainers/smooth_grad.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from explainers.utils.normalize_heatmap import process_heatmap_by_type
+from explainers.utils.slic import superpixel_mean_map
+import numpy as np
+import cv2
 
 
 class SmoothGrad(nn.Module):
@@ -14,6 +17,9 @@ class SmoothGrad(nn.Module):
         self.num_sample = config_dict.get('num_sample',10)
         self.var = config_dict.get('var',0.125)
         self.type = config_dict.get('type', 'both')  # type 추가
+        self.slic = config_dict.get('slic', False)
+        self.slic_size = config_dict.get('slic_size', 10)
+        self.slic_ruler = config_dict.get('slic_ruler', 10)
 
     def generate(self, input_tensor, class_idx=None):
         
@@ -43,22 +49,17 @@ class SmoothGrad(nn.Module):
         
         
         avg_gradients = total_gradients/self.num_sample
-        
-        # type에 따라 처리
-        if self.type == 'abs':
-            saliency = avg_gradients.abs().mean(dim=1)
-        elif self.type == 'positive':
-            saliency = F.relu(avg_gradients).mean(dim=1)
-        elif self.type == 'negative':
-            saliency = -F.relu(-avg_gradients).mean(dim=1)
-        elif self.type == 'both':
-            saliency = avg_gradients.mean(dim=1)
-        else:
-            saliency = avg_gradients.mean(dim=1)
-        
+        saliency = avg_gradients.mean(dim=1)  # shape: (B, H, W)
+       
         # utils의 통합 처리 함수 사용
         saliency = process_heatmap_by_type(saliency.squeeze(), self.type)
-
+        if self.slic is True:
+            print("SmoothGrad Slic Lets go")
+            sp_map, labels, sp_means = superpixel_mean_map(saliency, region_size=int(self.slic_size), ruler=float(self.slic_ruler))
+            sp_map_norm = (sp_map - sp_map.min()) / (sp_map.max() - sp_map.min() + 1e-8)
+            sp_map_uint8 = (sp_map_norm * 255).astype(np.uint8)
+            sp_map_color = cv2.applyColorMap(sp_map_uint8, cv2.COLORMAP_JET)
+            return sp_map_color  # (H, W, 3) RGB 이미지 반환
         return saliency
         
     def set_config_dict(self, config_dict):
@@ -66,4 +67,7 @@ class SmoothGrad(nn.Module):
         self.num_sample = config_dict.get('num_sample', self.num_sample)
         self.var = config_dict.get('var',self.var)
         self.type = config_dict.get('type', self.type)  # type 추가
+        self.slic = config_dict.get('slic', self.slic)
+        self.slic_size = config_dict.get('slic_size', self.slic_size)
+        self.slic_ruler = config_dict.get('slic_ruler', self.slic_ruler)
 
